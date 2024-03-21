20b91dd3246aa7ad19b5e4e36c6c016b73e4d873
YunSwain <destap@naver.com>
2024-03-21 20:14:54 +0900

    feat: RequestDTO 정의

---
+++ MovieFinder/Domain/DTOs/RequestDTO/DailyBoxOfficeRequestDTO.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct DailyBoxOfficeRequestDTO {
+    let key: String
+    let date: Date
+}
---
+++ MovieFinder/Domain/DTOs/RequestDTO/MovieInfoRequestDTO.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct MovieInfoRequestDTO {
+    let key: String
+    let code: String
+}
