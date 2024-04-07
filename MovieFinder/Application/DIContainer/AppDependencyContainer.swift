7b0f9f37499b7f659fab3ac70c0332339cd1372e
YunSwain <destap@naver.com>
2024-04-07 14:44:53 +0900

    chore: 기타

--- MovieFinder/Application/DIContainer/AppDependencyContainer.swift
+++ MovieFinder/Application/DIContainer/AppDependencyContainer.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+protocol AppDependencyContainer {
+    // TODO: 나중에 만들어보기
+}
+
+struct DefaultDIContainer: AppDependencyContainer {
+    
+}
--- MovieFinder/Application/MovieFinderApp.swift
+++ MovieFinder/Application/MovieFinderApp.swift
@@ -11,10 +11,12 @@ private let sessionManager = DefaultNetworkSessionManager()
 private let networkService = DefaultNetworkService(sessionManager: sessionManager)
 private let dailyBoxOfficeListRepository = DefaultDailyBoxOfficeListRepository(networkService: networkService)
 private let fetchDailyBoxOfficeUseCase = DefaultFetchDailyBoxOfficeListUseCase(repository: dailyBoxOfficeListRepository)
+private let fetchMovieDetailUseCase = DefaultFetchMovieDetailUseCase(repository: dailyBoxOfficeListRepository)
 
 @main
 struct MovieFinderApp: App {
     private let dailyBoxOfficeListViewModel = DailyBoxOfficeListViewModel(fetchDailyBoxOfficeListUseCase: fetchDailyBoxOfficeUseCase)
+    private let detailMovieInfoViewModel = DetailMovieInfoViewModel(fetchMovieDetailUseCase: fetchMovieDetailUseCase)
     
     var body: some Scene {
         WindowGroup {
