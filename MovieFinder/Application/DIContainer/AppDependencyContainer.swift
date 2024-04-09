ca6f84a01b19850e24eb5acbbc06b1c7d6d565d9
YunSwain <destap@naver.com>
2024-04-09 16:21:45 +0900

    refactor: 의존성 주입

--- MovieFinder/Application/DIContainer/AppDependencyContainer.swift
+++ MovieFinder/Application/DIContainer/AppDependencyContainer.swift
@@ -7,10 +7,38 @@
 
 import Foundation
 
-protocol AppDependencyContainer {
-    // TODO: 나중에 만들어보기
+protocol Resolver {
+    func resolve<T>(for type: T.Type) -> T?
 }
 
-struct DefaultDIContainer: AppDependencyContainer {
+protocol AppDependencyContainer {
+    func register<T>(_ instance: T)
+    func register<T>(for type: T.Type, _ handler: @escaping (Resolver) -> T)
     
+    func resolve<T>(for type: T.Type) -> T?
+}
+
+final class DefaultDependencyContainer {
+    // MARK: Properties
+    private var dependencies: [String: Any] = [:]
+}
+
+// MARK: AppDependencyContainer Confirmation
+extension DefaultDependencyContainer: AppDependencyContainer {
+    func register<T>(_ instance: T) {
+        let key = String(describing: instance.self)
+        dependencies[key] = instance
+    }
+    
+    func register<T>(for type: T.Type, _ handler: @escaping (Resolver) -> T) {
+        let key =  String(describing: type)
+        dependencies[key] = handler
+    }
+    
+    func resolve<T>(for type: T.Type) -> T? {
+        let key = String(describing: type)
+        
+        guard let value = dependencies[key] as? T else { return nil }
+        return value
+    }
 }
--- MovieFinder/Application/MovieFinderApp.swift
+++ MovieFinder/Application/MovieFinderApp.swift
@@ -7,20 +7,35 @@
 
 import SwiftUI
 
-private let sessionManager = DefaultNetworkSessionManager()
-private let networkService = DefaultNetworkService(sessionManager: sessionManager)
-private let dailyBoxOfficeListRepository = DefaultDailyBoxOfficeListRepository(networkService: networkService)
-private let fetchDailyBoxOfficeUseCase = DefaultFetchDailyBoxOfficeListUseCase(repository: dailyBoxOfficeListRepository)
-private let fetchMovieDetailUseCase = DefaultFetchMovieDetailUseCase(repository: dailyBoxOfficeListRepository)
-
 @main
 struct MovieFinderApp: App {
-    private let dailyBoxOfficeListViewModel = DailyBoxOfficeListViewModel(fetchDailyBoxOfficeListUseCase: fetchDailyBoxOfficeUseCase)
-    private let detailMovieInfoViewModel = DetailMovieInfoViewModel(fetchMovieDetailUseCase: fetchMovieDetailUseCase)
+    private let container: AppDependencyContainer = {
+        let container: AppDependencyContainer = DefaultDependencyContainer()
+        container.register(DefaultNetworkSessionManager())
+        container.register(for: DefaultNetworkService.self) { resolver in
+            DefaultNetworkService(sessionManager: resolver.resolve(for: DefaultNetworkSessionManager.self)!)
+        }
+        container.register(for: DefaultDailyBoxOfficeListRepository.self) { resolver in
+            DefaultDailyBoxOfficeListRepository(networkService: resolver.resolve(for: DefaultNetworkService.self)!)
+        }
+        container.register(for: DefaultFetchDailyBoxOfficeListUseCase.self) { resolver in
+            DefaultFetchDailyBoxOfficeListUseCase(repository: resolver.resolve(for: DefaultDailyBoxOfficeListRepository.self)!)
+        }
+        container.register(for: DefaultFetchMovieDetailUseCase.self) { resolver in
+            DefaultFetchMovieDetailUseCase(repository: resolver.resolve(for: DefaultDailyBoxOfficeListRepository.self)!)
+        }
+        container.register(for: DailyBoxOfficeListViewModel.self) { resolver in
+            DailyBoxOfficeListViewModel(fetchDailyBoxOfficeListUseCase: resolver.resolve(for: DefaultFetchDailyBoxOfficeListUseCase.self)!)
+        }
+        container.register(for: DetailMovieInfoViewModel.self) { resolver in
+            DetailMovieInfoViewModel(fetchMovieDetailUseCase: resolver.resolve(for: DefaultFetchMovieDetailUseCase.self)!)
+        }
+        return container
+    }()
     
     var body: some Scene {
         WindowGroup {
-            DailyBoxOfficeListView(vm: dailyBoxOfficeListViewModel)
+            DailyBoxOfficeListView(vm: container.resolve(for: DailyBoxOfficeListViewModel.self)!)
         }
     }
 }
